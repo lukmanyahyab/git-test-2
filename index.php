4c870c9525f041da6e27f85c2547b2102659186e
Lukman Yahya <alphabeticc@gmail.com>
2022-06-22 20:20:40 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt voluptate iure necessitatibus, numquam tenetur qui.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
