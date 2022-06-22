e112ac746c51414e17c4547d99a4137a1cdc4760
Lukman Yahya <alphabeticc@gmail.com>
2022-06-22 20:15:59 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt voluptate iure necessitatibus, numquam tenetur qui.</p>
 </body>
 </html>
